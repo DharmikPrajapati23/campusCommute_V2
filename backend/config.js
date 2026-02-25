9f2fd0d7aa7e2850d55384901998f092b147244c
Dharmik <dharmikj2306@gmail.com>
2026-02-25 18:58:22 +0530

    login fixed 4

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 
 const FRONTEND_URL = process.env.FRONTEND_URL;
+const LLM_URL = process.env.LLM_URL;
 
 module.exports = {
-  FRONTEND_URL
+  FRONTEND_URL,
+  LLM_URL
 };
